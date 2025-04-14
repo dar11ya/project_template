e06069a406c77e754091e55405170828fd6246f7
Dariya Omelchenko <dariyaomelchenko25@gmail.com>
2025-04-15 00:40:21 +0300

    Project is finished

--- app/io/output.py
+++ app/io/output.py
@@ -1,9 +1,20 @@
 def output_to_console(data):
-    """Outputting text to the console"""
-    print("Result:")
+    """
+    Prints the given data to the console.
+
+    Parameters:
+        data (str): Text to be printed.
+    """
+    print("Output:")
     print(data)
 
-def output_to_file_builtin(data):
-    """Writing to a file using built-in Python tools"""
-    with open("data/sample.txt", "a", encoding="utf-8") as file:
+def output_to_file_builtin(filename, data):
+    """
+    Appends the given data to a file.
+
+    Parameters:
+        filename (str): The path to the output file.
+        data (str): The content to write to the file.
+    """
+    with open(filename, "a", encoding="utf-8") as file:
         file.write(data + "\n")
